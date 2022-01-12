10a12e3d22d23c5fb09f614f266447fb1908759d
Andras <dlink.uaua@gmail.com>
2022-01-12 21:08:13 +0200

    changed to new test task

---
+++ README.md
@@ -0,0 +1,4 @@
+# defaults
+repo with defaults settings for IDEs and hello-worlds
+
+http://winlibs.com/
---
+++ build/src/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../bin/project.exe"
+  "../../bin/project.exe.manifest"
+  "../../bin/project.pdb"
+  "../../lib/libproject.dll.a"
+  "CMakeFiles/project.dir/main.cpp.obj"
+  "CMakeFiles/project.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
